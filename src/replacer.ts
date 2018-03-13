b4b6a94baa6e6152359b6594fa819b623f012833
Jack Roper <roper.jack.h@gmail.com>
2018-03-13 19:55:47 +0000

    Correct styling

--- src/replacer.ts
+++ src/replacer.ts
@@ -2,44 +2,43 @@
  * Simple class for regex text replacement.
  */
 export class Replacer {
+  private _matcher: RegExp;
+  private _replacement: string;
 
-    private _matcher: RegExp;
-    private _replacement : string;
+  constructor(matcher: RegExp, replacement: string) {
+    this._matcher = matcher;
+    this._replacement = replacement;
+  }
 
-    constructor(matcher: RegExp, replacement: string) {
-        this._matcher = matcher;
-        this._replacement = replacement;
-    }
-
-    /**
-     * Run a pattern replacement on the supplied text and return the result.
-     */
-    replace(text: string) {
-        return text.replace(this._matcher, this._replacement);
-    }
+  /**
+   * Run a pattern replacement on the supplied text and return the result.
+   */
+  replace(text: string) {
+    return text.replace(this._matcher, this._replacement);
+  }
 }
 
 /**
  * Return a string of #tabSize spaces.
  */
 function tabSizeToSpaces(tabSize: number) {
-    return new Array(tabSize + 1).join(' ');
+  return new Array(tabSize + 1).join(' ');
 }
 
 /**
  * Convenience class for replacing tabs with spaces.
  */
 export class TabReplacer extends Replacer {
-    constructor(tabSize: number) {
-        super(/\t/g, tabSizeToSpaces(tabSize));
-    }
+  constructor(tabSize: number) {
+    super(/\t/g, tabSizeToSpaces(tabSize));
+  }
 }
 
 /**
  * Convenience class for replacing spaces with tabs.
  */
 export class SpaceReplacer extends Replacer {
-    constructor(tabSize: number) {
-        super(new RegExp(tabSizeToSpaces(tabSize), 'g'), '\t');
-    }
+  constructor(tabSize: number) {
+    super(new RegExp(tabSizeToSpaces(tabSize), 'g'), '\t');
+  }
 }
