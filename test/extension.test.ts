df07139422b743506f7a855fcb7f455aa8cd8db8
Jack Roper <roper.jack.h@gmail.com>
2018-03-13 19:35:07 +0000

    Add text replacement tests

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -2,12 +2,6 @@ import * as assert from 'assert';
 import * as vscode from 'vscode';
 import * as myExtension from '../src/extension';
 
-
 suite("Whitespace Tests", () => {
-
-	test("Something 1", () => {
-		assert.equal(-1, [1, 2, 3].indexOf(5));
-		assert.equal(-1, [1, 2, 3].indexOf(0));
-	});
 
 });
